f12a54d08947480df8d27d762d8f596b7b69297c
Caio Tarifa <caio@formaweb.com.br>
2016-05-18 00:12:21 -0300

    socket error

--- db/schema.rb
+++ db/schema.rb
@@ -13,43 +13,40 @@
 
 ActiveRecord::Schema.define(version: 20150526001536) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "events", force: :cascade do |t|
-    t.boolean  "status"
-    t.string   "name"
-    t.string   "video_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "status",     limit: 1
+    t.string   "name",       limit: 255
+    t.string   "video_url",  limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "messages", force: :cascade do |t|
-    t.integer  "event_id"
-    t.integer  "user_id"
-    t.text     "message"
-    t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "event_id",   limit: 4
+    t.integer  "user_id",    limit: 4
+    t.text     "message",    limit: 65535
+    t.string   "image",      limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "",    null: false
+    t.string   "encrypted_password",     limit: 255, default: "",    null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "name"
-    t.string   "facebook_uid"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "name",                   limit: 255
+    t.string   "facebook_uid",           limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "online",                 default: false
-    t.boolean  "super_user",             default: true
+    t.boolean  "online",                 limit: 1,   default: false
+    t.boolean  "super_user",             limit: 1,   default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
